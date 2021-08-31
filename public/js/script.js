083d425af270154c8ccde55b5eed430d20760457
Giacche <giacchetti125@gmail.com>
2021-08-31 16:31:35 +0200

    Create peer element which represents current user

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
 const socket = io('/'); // Create our socket
 
-const videoGrid = document.getElementById('video-grid') // Find the Video-Grid element
+const videoGrid = document.getElementById('video-grid'); // Find the Video-Grid element
+
+// Creating a peer element which represents the current user
+const myPeer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '443'
+});
