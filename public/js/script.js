bcee6b3e8ac8e5660e908f7533f214dca46a8456
Giacche <giacchetti125@gmail.com>
2021-08-31 16:53:51 +0200

    function to stop and play user video with button

--- public/js/script.js
+++ public/js/script.js
@@ -104,3 +104,15 @@ const muteUnmute = () => {
       myVideoStream.getAudioTracks()[0].enabled = true;
     }
 }
+
+const playStop = () => {
+    console.log('object')
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo()
+    } else {
+      setStopVideo()
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
